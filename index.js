a030cc326d7a5db67c5f7b7d3fd491bf401d62c5
eosfelipe <escobedo.felipe@hotmail.com>
2022-08-25 13:06:35 -0500

    added the option to add the name of the site

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ const clipboardy = require('clipboardy')
 const log = console.log
 const createPassword = require('./utils/createPassword')
 const savePassword = require('./utils/savePassword')
-program.version('1.0.0').description('Simple Password Generator')
+program.version('1.1.0').description('Simple Password Generator')
 program
   .option('-l, --length <number>', 'length of password', '12')
   .option('-s, --save', 'save password to passwords.txt')
   .option('-nn, --no-numbers', 'remove numbers')
   .option('-ns, --no-symbols', 'remove symbols')
+  .option('-na, --name <string>', 'site to save password')
   .parse()
 
-const { length, save, numbers, symbols } = program.opts()
+const { length, save, numbers, symbols, name } = program.opts()
+
+// console.log(program.opts())
 
 // Get generated password
 const generatedPassword = createPassword(length, numbers, symbols)
 
 // Save to file
 if (save) {
-  savePassword(generatedPassword)
+  savePassword(generatedPassword, name)
 }
 
 // Copy to clipboard
